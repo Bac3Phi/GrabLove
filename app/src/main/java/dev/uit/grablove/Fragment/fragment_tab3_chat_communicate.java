231a0bd45dd0131ac932cc78b16eb0d1a644b1b6
gikizay <gkztest1@gmail.com>
2017-10-26 23:27:45 +0700

    Update Tab3_chat: communicate box,item_message

---
+++ app/src/main/java/dev/uit/grablove/Fragment/fragment_tab3_chat_communicate.java
@@ -0,0 +1,15 @@
+package dev.uit.grablove.Fragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import dev.uit.grablove.R;
+
+public class fragment_tab3_chat_communicate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_tab3_chat_communicate);
+    }
+}
