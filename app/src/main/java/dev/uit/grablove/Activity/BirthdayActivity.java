099f644c457876ee8356cc7ed3c07710cca3058e
Bac3Phi <gkztest1@gmail.com>
2017-11-10 19:31:44 +0700

    *Dob => Canlender=> pick datetime *Change background *random background in Splash.

--- app/src/main/java/dev/uit/grablove/Activity/BirthdayActivity.java
+++ app/src/main/java/dev/uit/grablove/Activity/BirthdayActivity.java
@@ -1,11 +1,25 @@
 package dev.uit.grablove.Activity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.rengwuxian.materialedittext.MaterialEditText;
+
+import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import dev.uit.grablove.R;
 import info.hoang8f.widget.FButton;
@@ -13,7 +27,10 @@ import info.hoang8f.widget.FButton;
 public class BirthdayActivity extends AppCompatActivity {
     private MaterialEditText etDob;
     private FButton btnNext;
-
+    TextView tvDob;
+    Calendar calendarDob;
+    int day, month,year;
+    ImageButton btnCalendarDob;
     static BirthdayActivity birthdayActivity;
 
     @Override
@@ -23,8 +40,33 @@ public class BirthdayActivity extends AppCompatActivity {
 
         birthdayActivity = this;
 
-        etDob = (MaterialEditText) findViewById(R.id.edtBirthdayBirthday);
+        tvDob= (TextView) findViewById(R.id.tvDob);
+        btnCalendarDob= (ImageButton) findViewById(R.id.btnCalenderBirthday) ;
+        calendarDob = Calendar.getInstance();
+
+        day= calendarDob.get(Calendar.DAY_OF_MONTH);
+        month= calendarDob.get(Calendar.MONTH);
+        year= calendarDob.get(Calendar.YEAR);
+
+        month = month +1;
+        tvDob.setText(day+"/"+month+"/"+year);
+
+
         btnNext = (FButton) findViewById(R.id.btnNextBirthday);
+
+        btnCalendarDob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog = new DatePickerDialog(BirthdayActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                    i1 = i1+1;
+                    tvDob.setText(i2+"/"+i1+"/"+i);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
 
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,10 +75,12 @@ public class BirthdayActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(getBaseContext(), AvatarActivity.class);
                 intent.putExtra("sex", strSex);
-                intent.putExtra("dob", etDob.getText().toString());
+                intent.putExtra("dob", tvDob.getText().toString());
                 startActivity(intent);
             }
         });
+
+
     }
 
     public static BirthdayActivity getInstance(){
--- app/src/main/java/dev/uit/grablove/Activity/SplashScreenActivity.java
+++ app/src/main/java/dev/uit/grablove/Activity/SplashScreenActivity.java
@@ -5,6 +5,10 @@ import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import dev.uit.grablove.Constants;
 import dev.uit.grablove.MainActivity;
@@ -13,11 +17,23 @@ import dev.uit.grablove.R;
 public class SplashScreenActivity extends AppCompatActivity {
     private final int SPLASH_DISPLAY_LENGTH = 2000;
 
-
+    ArrayList listBG= new ArrayList();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayoutSplashScreen);
+
+        listBG.add(R.drawable.background_firewatch_1);
+        listBG.add(R.drawable.background_firewatch_2);
+        listBG.add(R.drawable.background_firewatch_3);
+        listBG.add(R.drawable.background_firewatch_4);
+        listBG.add(R.drawable.background_firewatch_5);
+
+        Collections.shuffle(listBG);
+
+        relativeLayout.setBackgroundResource((Integer) listBG.get(0));
 
         new Handler().postDelayed(new Runnable(){
             @Override
